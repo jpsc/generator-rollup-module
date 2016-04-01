161b8c782e215f344c8f1efc97b6f8a963fca6c4
jpsc <joao@voorhoede.nl>
2016-04-01 23:15:33 +0200

    feat(template): add rollup plugin to use packages from node_modules

--- app/templates/config/rollup.js
+++ app/templates/config/rollup.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	entry: 'lib/index.js',
-	plugins: [babel()]
+	plugins: [babel(), nodeResolve({jsnext: true})]
 };
