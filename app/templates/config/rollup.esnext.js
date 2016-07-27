10fb705ccf5d906c521b2d67953f7ee718c6b625
jpsc <joao@voorhoede.nl>
2016-07-27 14:00:26 +0200

    chore(deps): update template dependecies

--- app/templates/config/rollup.esnext.js
+++ app/templates/config/rollup.esnext.js
@@ -3,6 +3,6 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 export default {
 	entry: 'lib/index.js',
 	plugins: [nodeResolve({jsnext: true})],
-	format: 'es6',
+	format: 'es',
 	dest: 'dist/<%= moduleName %>.js'
 };
